7598da2576bc4bb0e40c62b63c103671cf05805e
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-10-23 22:17:35 +0800

    added users and user query

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,6 +4,31 @@ let users = [
   {
     id: "id-1",
     name: "Ankit",
+    bio: "GraphQL Develoepr",
+  },
+  {
+    id: "id-2",
+    name: "Ankit Kumar",
+    bio: "Node Develoepr",
+  },
+  {
+    id: "id-3",
+    name: "Ankit Kumar",
+    bio: "Node Develoepr",
+  },
+  {
+    id: "id-4",
+    name: "Ankit",
+    bio: "Node Develoepr",
+  },
+  {
+    id: "id-5",
+    name: "Ankit",
+    bio: "Node Develoepr",
+  },
+  {
+    id: "id-6",
+    name: "Ankit",
     bio: "Node Develoepr",
   },
 ];
@@ -13,6 +38,10 @@ let count = users.length;
 const resolvers: IResolvers = {
   Query: {
     helloWolrd: () => "Hello World from Apollo Server",
+    users: () => users,
+    user: (parent, args) => {
+      return users.find((user) => user.id === args.id);
+    },
   },
   Mutation: {
     addUser: (parent, args) => {
